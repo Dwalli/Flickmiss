a3ae4dbe6c012a65fd80b4c4c9e8c3a114fdbf1a
Dwalli <92029006+Dwalli@users.noreply.github.com>
2024-04-23 16:36:01 +0100

    Movement Unoptimized

--- Assets/Scrips/PlayerMovments.cs
+++ Assets/Scrips/PlayerMovments.cs
@@ -7,54 +7,129 @@ using UnityEngine;
 public class PlayerMovments : MonoBehaviour
 {
     RaycastHit2D findNodeHit;
-    public GameObject node;
     public Transform player;
 
     private Vector3 newLocation;
-    private Vector2 CurrentLocation;
     [SerializeField]private bool canMove = true;
+
+    [SerializeField] private float clock;
+    [SerializeField] private float resetClcok = 1;
 
     private Vector2[] locatedNodes = new Vector2[4]; // array of new possible node locations
     private Vector3[] directions = { new Vector3(1, 0, 0), new Vector3(-1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, -1, 0) }; // right left up down
 
     public float speed = 0.2f;
 
+    public enum ActiveNodes { Normal, Inverted, Teleport }
+    public ActiveNodes node = ActiveNodes.Normal;
+
     // Start is called before the first frame update
     void Start()
     {
         GetNodeLocations();
+        clock = resetClcok;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void FixedUpdate()
     {
-        createRay();
         DrawRays();
+        ActivatedNode(node);
+
     }
 
-    void createRay()
+ 
+    private void ActivatedNode(ActiveNodes activeNodes)
+    {
+        switch (activeNodes)
+        {
+            case ActiveNodes.Normal: PlayerMove(); break;
+
+            case ActiveNodes.Inverted: PlayerMoveInverted(); break;
+
+            case ActiveNodes.Teleport: PlayerMoveTeleport(); break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Inverted")) { ActivatedNode(node = ActiveNodes.Inverted); Debug.Log("Inverted"); }
+
+        if (collision.gameObject.CompareTag("Teleport")) { ActivatedNode(node = ActiveNodes.Teleport); Debug.Log("Teleport"); }
+
+        if (collision.gameObject.CompareTag("Normal")) { ActivatedNode(node = ActiveNodes.Normal); Debug.Log("Normal"); }
+    }
+
+    void PlayerMove()
     {
         // getting the user inputs
-        if (Input.GetKey(KeyCode.D) && canMove == true) { newLocation = locatedNodes[0]; canMove = false; } 
+        if (Input.GetKey(KeyCode.D) && canMove == true) { newLocation = locatedNodes[0]; canMove = false; }
         if (Input.GetKey(KeyCode.A) && canMove == true) { newLocation = locatedNodes[1]; canMove = false; }
         if (Input.GetKey(KeyCode.W) && canMove == true) { newLocation = locatedNodes[2]; canMove = false; }
         if (Input.GetKey(KeyCode.S) && canMove == true) { newLocation = locatedNodes[3]; canMove = false; }
 
+        if (player.position == newLocation)
+        {
+            clock -= Time.deltaTime;
+            if (clock <= 0) 
+            {  
+                clock = resetClcok;
+                canMove = true;
+            }
+      
+            GetNodeLocations();
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, newLocation, speed);
 
-        Debug.Log("can move to: " + newLocation);
+    }
 
+    // has the player teleport to a selected node
+    void PlayerMoveTeleport()
+    {
+        // getting the user inputs
+        if (Input.GetKey(KeyCode.D) && canMove == true) { newLocation = locatedNodes[0]; canMove = false; }
+        if (Input.GetKey(KeyCode.A) && canMove == true) { newLocation = locatedNodes[1]; canMove = false; }
+        if (Input.GetKey(KeyCode.W) && canMove == true) { newLocation = locatedNodes[2]; canMove = false; }
+        if (Input.GetKey(KeyCode.S) && canMove == true) { newLocation = locatedNodes[3]; canMove = false; }
 
         if (player.position == newLocation)
         {
-            canMove = true;
+            clock -= Time.deltaTime;
+            if (clock <= 0)
+            {
+                clock = resetClcok;
+                canMove = true;
+            }
             GetNodeLocations();
         }
+
+        transform.position = newLocation;
+
+    }
+
+    // inverted controls
+    void PlayerMoveInverted()
+    {
+        // getting the user inputs
+        if (Input.GetKey(KeyCode.D) && canMove == true) { newLocation = locatedNodes[1]; canMove = false; }
+        if (Input.GetKey(KeyCode.A) && canMove == true) { newLocation = locatedNodes[0]; canMove = false; }
+        if (Input.GetKey(KeyCode.W) && canMove == true) { newLocation = locatedNodes[3]; canMove = false; }
+        if (Input.GetKey(KeyCode.S) && canMove == true) { newLocation = locatedNodes[2]; canMove = false; }
+
+        if (player.position == newLocation)
+        {
+            clock -= Time.deltaTime;
+            if (clock <= 0)
+            {
+                clock = resetClcok;
+                canMove = true;
+            }
+            GetNodeLocations();
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, newLocation, speed);
+
     }
 
     // make a loop to find nodes in 4 diffrent directions
